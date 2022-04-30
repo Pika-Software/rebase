77d5ebf0b1f10db4f8c4f0df15f479c50caf3c45
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-05-01 01:11:42 +0400

    Depricated

--- gamemodes/base/gamemode/shared/other.lua
+++ gamemodes/base/gamemode/shared/other.lua
@@ -35,16 +35,3 @@ end
 function GM:GravGunPickupAllowed( ply, ent )
 	return true
 end
-
-timer.Simple(0, function()
-	if (SERVER) then
-		concommand.Remove("lua_find")
-		concommand.Remove("lua_findhooks")
-	else
-		concommand.Remove("lua_find_cl")
-		concommand.Remove("lua_findhooks_cl")
-	end
-
-	-- Edit Variable
-	-- net.ReceiveRemove("editvariable")
-end)
