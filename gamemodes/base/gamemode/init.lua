b7fa90456f83217e110fac88916b3297e22952db
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-04-30 12:11:29 +0400

    Loading Optimization

--- gamemodes/base/gamemode/init.lua
+++ gamemodes/base/gamemode/init.lua
@@ -1,32 +1,7 @@
-local AddCSLuaFile = AddCSLuaFile
-local include = include
-
-AddCSLuaFile("shared.lua")
 include("shared.lua")
-
---  //      Client      //
-AddCSLuaFile("client/2d_render.lua")
-AddCSLuaFile("client/3d_render.lua")
-AddCSLuaFile("client/chat.lua")
-AddCSLuaFile("client/derma.lua")
-AddCSLuaFile("client/entity.lua")
-AddCSLuaFile("client/mouse_control.lua")
-AddCSLuaFile("client/movement.lua")
-AddCSLuaFile("client/other.lua")
-AddCSLuaFile("client/player_camera.lua")
-AddCSLuaFile("client/teams.lua")
-AddCSLuaFile("client/viewmodel.lua")
-
-AddCSLuaFile("shared/entity.lua")
-AddCSLuaFile("shared/other.lua")
-AddCSLuaFile("shared/player.lua")
-AddCSLuaFile("shared/player_class/player_default.lua")
-
---	//	    Includes	//
 include("server/other.lua")
 include("server/player.lua")
 
---  //      Server      //
 function GM:Initialize()
 end
 
