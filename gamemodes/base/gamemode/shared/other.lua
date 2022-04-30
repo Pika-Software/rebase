e079823351ce0ca7434f748566dc07133f08bc20
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-04-30 12:09:43 +0400

    Other [SERVER]

--- gamemodes/base/gamemode/shared/other.lua
+++ gamemodes/base/gamemode/shared/other.lua
@@ -1,17 +1,38 @@
+-- Extra
+function net.ReceiveRemove( name )
+	net.Receivers[ name:lower() ] = nil
+end
+
+do
+	local list_GetForEdit = list.GetForEdit
+	local ipairs = ipairs
+
+	function list.Remove( name, key )
+		local tbl = list_GetForEdit( name )
+		if (key == nil) then
+			for key, value in ipairs( tbl ) do
+				tbl[ key ] = nil
+			end
+		else
+			tbl[ key ] = nil
+		end
+	end
+end
+
 -- PhysGun
-function GM:PhysgunPickup(ply, ent)
+function GM:PhysgunPickup( ply, ent )
 	return true
 end
 
-function GM:PhysgunDrop(ply, ent)
+function GM:PhysgunDrop( ply, ent )
 end
 
 -- GravityGun
-function GM:GravGunPunt(ply, ent)
+function GM:GravGunPunt( ply, ent )
 	return true
 end
 
-function GM:GravGunPickupAllowed(ply, ent)
+function GM:GravGunPickupAllowed( ply, ent )
 	return true
 end
 
@@ -25,5 +46,5 @@ timer.Simple(0, function()
 	end
 
 	-- Edit Variable
-	net.ReceiveRemove("editvariable")
+	-- net.ReceiveRemove("editvariable")
 end)
