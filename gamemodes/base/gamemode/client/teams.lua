be388f84dbff25af47a79a4fc1c4a283805be786
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-04-30 19:36:35 +0400

    deprecated

--- gamemodes/base/gamemode/client/teams.lua
+++ gamemodes/base/gamemode/client/teams.lua
@@ -5,21 +5,6 @@ end
 function GM:HideTeam()
 end
 
--- Team Color
-do
-	local TEAM_UNASSIGNED = TEAM_UNASSIGNED
-	function GM:GetTeamColor( ent )
-		return self:GetTeamNumColor( (ent.Team == nil) and TEAM_UNASSIGNED or ent:Team() )
-	end
-end
-
-do
-	local team_GetColor = team.GetColor
-	function GM:GetTeamNumColor( num )
-		return team_GetColor( num )
-	end
-end
-
 -- Player Class Changed
 do
 
