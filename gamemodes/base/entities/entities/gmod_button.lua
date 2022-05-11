a90f89f9d2d36c796a03a9d84f801e445c597bab
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-05-11 23:18:26 +0400

    Button

--- gamemodes/base/entities/entities/gmod_button.lua
+++ gamemodes/base/entities/entities/gmod_button.lua
@@ -13,27 +13,23 @@ function ENT:SetupDataTables()
 	self:NetworkVar("String", 0, "Label", { KeyName = "lbl", Edit = { type = "Generic", order = 2, title = "#tool.button.text" } })
 
 	if (SERVER) then
-		self:SetOn(false)
-		self:SetIsToggle(false)
+		self:SetOn( false )
+		self:SetIsToggle( false )
 	end
 
 end
 
 function ENT:Initialize()
-
 	if (SERVER) then
 
-		self:PhysicsInit(SOLID_VPHYSICS)
-		self:SetMoveType(MOVETYPE_VPHYSICS)
-		self:SetSolid(SOLID_VPHYSICS)
-		self:SetUseType(ONOFF_USE)
+		self:PhysicsInit( SOLID_VPHYSICS )
+		self:SetMoveType( MOVETYPE_VPHYSICS )
+		self:SetSolid( SOLID_VPHYSICS )
+		self:SetUseType( ONOFF_USE )
 
 	else
-
 		self.PosePosition = 0
-
 	end
-
 end
 
 function ENT:GetOverlayText()
@@ -54,64 +50,62 @@ function ENT:GetOverlayText()
 
 end
 
-function ENT:Use(activator, caller, type, value)
+function ENT:Use( ply, caller, type, value )
+	if ply:IsPlayer() then
+		if self:GetIsToggle() then
+			if (type == USE_ON) then
+				self:Toggle( not self:GetOn(), ply )
+			end
 
-	if (!activator:IsPlayer()) then return end -- Who the frig is pressing this shit!?
-
-	if (self:GetIsToggle()) then
-
-		if (type == USE_ON) then
-			self:Toggle(!self:GetOn(), activator)
+			return
 		end
-		return
 
+		-- Someone is already using this button
+		if IsValid( self.LastUser ) then
+			return
+		end
+
+		--
+		-- Switch off
+		--
+		if self:GetOn() then
+			self:Toggle( false, ply )
+			return
+		end
+
+		--
+		-- Switch on
+		--
+		self:Toggle( true, ply )
+		self:NextThink( CurTime() )
+		self.LastUser = ply
 	end
-
-	if (IsValid(self.LastUser)) then return end -- Someone is already using this button
-
-	--
-	-- Switch off
-	--
-	if (self:GetOn()) then
-		self:Toggle(false, activator)
-		return
-	end
-
-	--
-	-- Switch on
-	--
-	self:Toggle(true, activator)
-	self:NextThink(CurTime())
-	self.LastUser = activator
-
 end
 
 function ENT:Think()
 
 	-- Add a world tip if the player is looking at it
-	self.BaseClass.Think(self)
+	self.BaseClass.Think( self )
 
 	-- Update the animation
 	if (CLIENT) then
-
 		self:UpdateLever()
-
 	end
 
 	--
 	-- If the player looks away while holding down use it will stay on
 	-- Lets fix that..
 	--
-	if (SERVER && self:GetOn() && !self:GetIsToggle()) then
 
-		if (!IsValid(self.LastUser) || !self.LastUser:KeyDown(IN_USE)) then
+	if (SERVER) and not self:GetIsToggle() then
+		if self:GetOn() then
+			if IsValid( self.LastUser ) and self.LastUser:KeyDown( IN_USE ) then
+				self:Toggle( false )
+				self.LastUser = nil
+			end
 
-			self:Toggle(false, self.LastUser)
-			self.LastUser = nil
-
+			self:NextThink( CurTime() )
 		end
-
-		self:NextThink(CurTime())
 
 	end
 
@@ -120,33 +114,21 @@ end
 --
 -- Makes the button trigger the keys
 --
-function ENT:Toggle(bEnable, ply)
-
+function ENT:Toggle(bEnable)
 	if (bEnable) then
-
-		numpad.Activate(self:GetPlayer(), self:GetKey(), true)
-		self:SetOn(true)
-
+		numpad.Activate( self:GetPlayer(), self:GetKey(), true )
+		self:SetOn( true )
 	else
-
-		numpad.Deactivate(self:GetPlayer(), self:GetKey(), true)
-		self:SetOn(false)
-
+		numpad.Deactivate( self:GetPlayer(), self:GetKey(), true )
+		self:SetOn( false )
 	end
-
 end
 
 --
 -- Update the lever animation
 --
 function ENT:UpdateLever()
-
-	local TargetPos = 0.0
-	if (self:GetOn()) then TargetPos = 1.0 end
-
-	self.PosePosition = math.Approach(self.PosePosition, TargetPos, FrameTime() * 5.0)
-
+	self.PosePosition = math.Approach( self.PosePosition, self:GetOn() and 1 or 0, FrameTime() * 5.0 )
 	self:SetPoseParameter("switch", self.PosePosition)
 	self:InvalidateBoneCache()
-
 end
