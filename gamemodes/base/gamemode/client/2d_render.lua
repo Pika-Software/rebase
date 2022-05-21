80b56ebdc7ea3359b112f8cffef3e5906db569ab
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-05-21 20:09:31 +0400

    DarkRP Support ~ 1.3.6

--- gamemodes/base/gamemode/client/2d_render.lua
+++ gamemodes/base/gamemode/client/2d_render.lua
@@ -10,6 +10,9 @@ function GM:HUDPaintBackground()
 end
 
 function GM:HUDPaint()
+	hook.Run( "HUDDrawTargetID" )
+	hook.Run( "HUDDrawPickupHistory" )
+	hook.Run( "DrawDeathNotice", 0.85, 0.04 )
 end
 
 function GM:PostDrawHUD()
--- gamemodes/base/gamemode/shared.lua
+++ gamemodes/base/gamemode/shared.lua
@@ -2,7 +2,7 @@ GM.Name = "Re-Base"
 GM.Author = "Pika Software"
 GM.Email = "prikolmen@pika-soft.ru"
 GM.Website = "https://pika-soft.ru"
-GM.Version = "1.3.5"
+GM.Version = "1.3.6"
 
 do
 
