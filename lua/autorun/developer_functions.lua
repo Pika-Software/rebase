224ab607a56f4d992d5c33743e8ab221b452897f
PrikolMen <olegis019@gmail.com>
2023-04-23 05:06:36 +0400

    little rewrited

--- lua/autorun/developer_functions.lua
+++ lua/autorun/developer_functions.lua
@@ -1,6 +1,8 @@
-if (CLIENT) and (spawnmenu ~= nil) and (spawnmenu.AddToolMenuOption ~= nil) then
-    local original = spawnmenu.AddToolMenuOption
-    function spawnmenu.AddToolMenuOption( tab, ... )
-        return original( (tab == "Options") and "Utilities" or tab, ... )
-    end
+if not CLIENT then return end
+if not spawnmenu then return end
+if not spawnmenu.AddToolMenuOption then return end
+
+local func = spawnmenu.AddToolMenuOption
+function spawnmenu.AddToolMenuOption( tab, ... )
+    return func( ( tab == "Options" ) and "Utilities" or tab, ... )
 end
